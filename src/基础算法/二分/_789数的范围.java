dec4d9adc4976ee0117be1d42e1ce40281d98aef
Y-hai <yanghai12721@163.com>
2021-12-01 16:59:25 +0800

    二分查找

--- src/基础算法/二分/_789数的范围.java
+++ src/基础算法/二分/_789数的范围.java
@@ -1,8 +1,42 @@
 package 基础算法.二分;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _789数的范围 {
-    public static void main(String[] args) {
-        
-        System.out.println();
+    static final int N = (int) 1e5 + 10;
+    static int[] a = new int[N];
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String[] arr = in.readLine().split(" ");
+        int n = Integer.parseInt(arr[0]);
+        int q = Integer.parseInt(arr[1]);
+        String[] str = in.readLine().split(" ");
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(str[i]);
+
+        while (q-- > 0) {
+            int k = Integer.parseInt(in.readLine());
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int mid = l + r >> 1;
+                if (a[mid] >= k) r = mid;
+                else l = mid + 1;
+            }
+            int left = l;
+            if (a[l] != k)
+                System.out.println("-1 -1");
+            else {
+                l = 0;
+                r = n - 1;
+                while (l < r) {
+                    int mid = l + r + 1 >> 1;
+                    if (a[mid] <= k) l = mid;
+                    else r = mid - 1;
+                }
+                System.out.println(left + " " + l);
+            }
+        }
     }
 }
