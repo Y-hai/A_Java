18b39e6e8148fc133ca45bd13ce2f77caf164210
Y-hai <yanghai12721@163.com>
2021-12-03 10:32:34 +0800

    多线程并发

--- src/多线程/实现Runnable接口.java
+++ src/多线程/实现Runnable接口.java
@@ -1,11 +1,22 @@
 package 多线程;
 
+/*
+    第二种实现方式
+    这种方式更常用，因为它面向接口编程，更加灵活，还可以实现其它抽象类
+ */
 public class 实现Runnable接口 {
     public static void main(String[] args) {
         // 创建一个可运行的对象
-        MyRunnable r = new MyRunnable();
+//        MyRunnable r = new MyRunnable();
         // 将可运行对象封装成一个线程对象
-        Thread t = new Thread(r);
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 1000; i++) {
+                    System.out.println("分支线程--->" + i);
+                }
+            }
+        });
         // 启动线程
         t.start();
         for (int i = 1; i <= 1000; i++) {
@@ -15,11 +26,11 @@ public class 实现Runnable接口 {
 }
 
 // 这并不是一个线程类，是一个可运行的类。它还不是一个线程。
-class MyRunnable implements Runnable {
-    @Override
-    public void run() {
-        for (int i = 1; i <= 1000; i++) {
-            System.out.println("分支线程--->" + i);
-        }
-    }
-}
+//class MyRunnable implements Runnable {
+//    @Override
+//    public void run() {
+//        for (int i = 1; i <= 1000; i++) {
+//            System.out.println("分支线程--->" + i);
+//        }
+//    }
+//}
--- src/多线程/重写run方法.java
+++ src/多线程/重写run方法.java
@@ -1,13 +1,13 @@
 package 多线程;
 
 /*
-    多线程第一种实现方法：重写Thread类的run方法
+    多线程第一种实现方法：重写Thread类的run方法，不推荐
     代码永远都是从上到下顺序执行
  */
 public class 重写run方法 {
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
-        myThread.run(); // 单线程
+//        myThread.run(); // 单线程
         myThread.start(); // 会开辟新的栈空间
         for (int i = 1; i <= 1000; i++) {
             System.out.println("主线程--->" + i);
@@ -16,6 +16,7 @@ public class 重写run方法 {
     }
 }
 
+// 重写Thread抽象类中的run方法
 class MyThread extends Thread {
     @Override
     public void run() {
